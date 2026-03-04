f89acdc69de58df7dec4e7b41d3078af94225d83
jiwonll <166104093+jiwonll@users.noreply.github.com>
2026-03-05 01:52:41 +0900

    ListPage 작업 중

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { theme } from "@/styles/theme";
 import GlobalStyle from "@/styles/GlobalStyle";
 
 import HomePage from "@/pages/HomePage";
+import ListPage from "@/pages/ListPage";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<HomePage />} />
+          <Route path="/list" element={<ListPage />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
