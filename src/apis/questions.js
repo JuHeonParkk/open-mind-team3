ce44bbede6254188458c3bbc234f861ee9f9eb09
JuHeonParkk <ju10913@naver.com>
2026-03-05 13:26:30 +0900

    feat: questions 목록 조회 및 생성 API 함수 구현

---
+++ src/apis/questions.js
@@ -0,0 +1,31 @@
+import instance from "@/apis/instance";
+
+export async function getQuestions(subjectId) {
+  subjectId = 13312; // 테스트용 고정값
+  try {
+    const response = await instance.get(`/subjects/${subjectId}/questions/`, {
+      params: {
+        limit: 8,
+        offset: 0,
+      },
+    });
+    console.log("질문목록:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching questions:", error);
+    throw error;
+  }
+}
+
+export async function postQuestions(subjectId, questionContent) {
+  subjectId = 13312; // 테스트용 고정값
+  try {
+    const response = await instance.post(`/subjects/${subjectId}/questions/`, {
+      content: questionContent,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error creating question:", error);
+    throw error;
+  }
+}
