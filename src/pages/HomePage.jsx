32c0e80d818a657465f72a30a5eee7d54f808b94
Hanpla <hanpla25@gmail.com>
2026-03-07 11:12:11 +0900

    feat: 토스트 컴포넌트 추가(react-hot-toast 라이브러리 설치)

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,14 @@
+import { theme } from "@/styles/theme";
+import { openToast } from "@/utils/toast";
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  const handleBtnClick = () => {
+    openToast("토스트 출력", theme.colors.red);
+  };
+
+  return (
+    <div>
+      <button onClick={handleBtnClick}>토스트 클릭</button>
+    </div>
+  );
 }
