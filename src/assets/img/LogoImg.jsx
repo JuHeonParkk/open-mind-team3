7eda76b35af7cd3c37271e6a9ae6cb46c5fed771
Hanpla <hanpla25@gmail.com>
2026-03-12 14:14:05 +0900

    chore: memo 제거

--- src/assets/img/LogoImg.jsx
+++ src/assets/img/LogoImg.jsx
@@ -1,9 +1,5 @@
-import { memo } from "react";
-
 import Logo from "./Logo.svg";
 
-function LogoImg({ width = "100%", ...props }) {
+export default function LogoImg({ width = "100%", ...props }) {
   return <img src={Logo} width={width} {...props} />;
 }
-
-export default memo(LogoImg);
