a54d4daa3ad5c054f3d905950ebe565d84ce9274
JuHeonParkk <ju10913@naver.com>
2026-03-15 01:57:21 +0900

    feat: 답변 영역 스켈레톤 추가 및 초기 렌더링 3개로 변경

--- src/components/containers/Question/QuestionList.jsx
+++ src/components/containers/Question/QuestionList.jsx
@@ -8,6 +8,7 @@ import { MessagesIcon } from "@/assets/icons/MessagesIcon";
 import QuestionCount from "@/components/containers/Question/QuestionCount/QuestionCount";
 import QuestionItems from "@/components/containers/Question/QuestionItems/QuestionItems";
 import PostModal from "@/components/containers/PostModal/PostModal";
+import QuestionListSkeleton from "@/components/containers/Question/QuestionListSkeleton/QuestionListSkeleton";
 
 import * as S from "@/components/containers/Question/QuestionList.style";
 
@@ -36,7 +37,7 @@ export default function QuestionList({ subjectId, isAnswer }) {
   }, [fetchQuestions]);
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <QuestionListSkeleton />;
   }
 
   return (
