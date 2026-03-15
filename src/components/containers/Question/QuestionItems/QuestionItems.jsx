4aaa0b363aa71fb23e39605beafd5f68d62c79be
JuHeonParkk <ju10913@naver.com>
2026-03-15 16:59:32 +0900

    feat: postModal과 emptyQuestion 파일에 subjectData 연결

--- src/components/containers/Question/QuestionItems/QuestionItems.jsx
+++ src/components/containers/Question/QuestionItems/QuestionItems.jsx
@@ -1,11 +1,12 @@
 import EmptyQuestion from "@/components/containers/Question/EmptyQuestion/EmptyQuestion";
 import QuestionItem from "@/components/containers/Question/QuestionItem/QuestionItem";
 
-<<<<<<< HEAD
-export default function QuestionItems({ questions, isAnswer, fetchQuestions }) {
-=======
-export default function QuestionItems({ questions, isAnswer, subjectData }) {
->>>>>>> e590fe5 (feat: postModal과 emptyQuestion 파일에 subjectData 연결)
+export default function QuestionItems({
+  questions,
+  isAnswer,
+  fetchQuestions,
+  subjectData,
+}) {
   return (
     <>
       {questions.length === 0 ? (
