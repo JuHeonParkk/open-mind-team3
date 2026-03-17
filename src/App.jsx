5b0714be50f540ca99ce895a637ffa2cbb983419
jiwonll <166104093+jiwonll@users.noreply.github.com>
2026-03-17 17:40:31 +0900

    refactor: 스피너 관련  스타일 코드 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider, styled } from "styled-components"; // styled 추가
 import { Toaster } from "react-hot-toast";
-import React, { Suspense, lazy } from "react"; // 1. Suspense와 lazy 추가
+import React, { Suspense, lazy } from "react";
 
 import { theme } from "@/styles/theme";
 import GlobalStyle from "@/styles/GlobalStyle";
 import { PublicRoute } from "@/components/routes/PublicRoute";
 import { LoadingSpinner } from "@/assets/icons/LoadingSpinnerIcon";
+
+const LoadingWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+`;
 
 const HomePage = lazy(() => import("@/pages/HomePage"));
 const ListPage = lazy(() => import("@/pages/ListPage/ListPage"));
@@ -19,7 +27,13 @@ export default function App() {
       <Toaster position="bottom-center" containerStyle={{ bottom: 60 }} />
       <GlobalStyle />
       <BrowserRouter>
-        <Suspense fallback={<LoadingSpinner/>}>
+        <Suspense
+          fallback={
+            <LoadingWrapper>
+              <LoadingSpinner size={40} />
+            </LoadingWrapper>
+          }
+        >
           <Routes>
             <Route element={<PublicRoute />}>
               <Route index element={<HomePage />} />
